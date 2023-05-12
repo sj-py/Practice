708169cd66e9a67fdcd3ccbfee53179d21d4f1ff
Seongjin Kim <mansj98@naver.com>
2023-05-12 18:07:04 +0900

    why

--- 컴퓨터프로그래밍기초/ch12.py
+++ 컴퓨터프로그래밍기초/ch12.py
@@ -1,5 +1,33 @@
 #print("안녕 파이썬! 처음이야")
-a = 100
-print(a)
-print("%s"%"hello")
-print("%10s"%"hihi")
+# a = 100
+# print(a)
+# print("%s"%"hello")
+# print("%10s"%"hihi")
+
+# 계산기
+# a = int(input("첫 번째 숫자를 입력하세오 : "))
+# b = int(input("두 번째 숫자를 입력하세오 : "))
+# result = a + b
+# print(a, "+", b, "=", result)
+# result = a - b
+# print(a, "-", b, "=", result)
+# result = a * b
+# print(a, "*", b, "=", result)
+# result = a / b
+# print(a, "/", b, "=", result)
+
+# turtle
+import turtle
+
+turtle.shape('turtle')
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+
+turtle.done()
