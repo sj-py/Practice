f64297beac6f51b5817cf5d4af62bf7f2133ae2f
Seongjin Kim <mansj98@naver.com>
2023-05-14 20:33:32 +0900

    pratice

--- wikidocs/181~190_반복문.py
+++ wikidocs/181~190_반복문.py
@@ -1,1 +1,58 @@
-# 181
+# 181 2차원 리스트
+apart = [['101호', '102호'],
+         ['201호', '202호'],
+         ['301호', '302호']]
+
+# 182
+stock = [['시가', 100, 200, 300],
+         ['종가', 80, 210, 330]]
+
+# 183
+stock = {"시가" : [100, 200, 300], "종가" : [80, 210, 330]}
+
+# 184 
+stock = {"10/10" : [80, 110, 70, 90], "10/11" : [210, 230, 190, 200]}
+
+# 185
+apart = [['101호', '102호'],
+         ['201호', '202호'],
+         ['301호', '302호']]
+for i in apart :
+    for j in i :
+        print(j)
+
+print("-" * 10)   
+
+# 186
+for i in apart[::-1] :
+    for j in i :
+        print(j)
+    
+print('-' * 10)
+
+# 187
+for i in apart[::-1] : 
+    for j in i[::-1] :
+        print(j)
+        
+print("-" * 10)
+
+# 188
+for i in apart :
+    for j in i :
+        print(j)
+        print("----")
+        
+print("-" * 10)
+
+# 189
+for i in apart :
+    for j in i :
+        print(j)
+    print("-" * 4)
+    
+# 190
+for i in apart :
+    for j in i :
+        print(j)
+print("----")
---
+++ wikidocs/191~200_반복문.py
@@ -0,0 +1,81 @@
+# 191 OHLC(open/high/low/close) fee = 0.014%
+data = [
+    [ 2000,  3050,  2050,  1980],
+    [ 7500,  2050,  2050,  1980],
+    [15450, 15050, 15550, 14900]
+]
+
+for i in data :
+    for j in i :
+        print(j * 1.00014)
+        
+print("-" * 10)
+
+# 192
+for i in data :
+    for j in i :
+        print(j * 1.00014)
+    print("----")
+    
+# 193
+result = []
+for i in data :
+    for j in i :
+        result.append(j * 1.00014)
+print(result)
+
+print("-" * 10)
+
+# 194
+result = []
+for i in data :
+    sub_list = []
+    for j in i :
+        sub_list.append(j * 1.00014)
+    result.append(sub_list)
+print(result)
+
+print("-" * 10)
+
+# 195 종가 출력
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+
+for i in ohlc :
+    if "close" in i :
+        a = i.index("close")
+    if type(i[0]) == int :
+        print(i[a])
+    
+print("-" * 10)
+
+# 196
+for i in ohlc :
+    if "close" in i :
+        a = i.index("close")
+    if type(i[0]) == int :
+        if i[a] > 150 :
+            print(i[a])
+            
+print("-" * 10)
+
+# 197
+for i in ohlc :
+    if "open" in i : a = i.index("open")
+    if "close" in i : b = i.index("close")
+    if type(i[0]) == int and i[a] <= i[b] : print(i[b])
+    
+print("-" * 10)
+
+# 198
+volatility = []
+for i in ohlc :
+    if "high" in i : a = i.index("high")
+    if "low" in i : b = i.index("low")
+    if type(i[0]) == int :
+        volatility.append(i[a] - i[b])
+print(volatility)
+
+print("-" * 10)
