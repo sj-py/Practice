fb569aea7eab5ae55dbf02bec5e51b52ead01b40
Seongjin Kim <mansj98@naver.com>
2023-05-14 22:38:29 +0900

    pratice

---
+++ HYCU/코딩교육/12차시.py
@@ -0,0 +1,42 @@
+# 해보기 1
+# def getVal() :
+#     a = int(input('val : '))
+#     return a
+
+# def add() :
+#     a = getVal()
+#     b = getVal()
+#     return a + b
+
+# def sub() :
+#     a = getVal()
+#     b = getVal()
+#     return a - b
+
+# def mul() :
+#     a = getVal()
+#     b = getVal()
+#     return a * b
+
+# def div() :
+#     a = getVal()
+#     b = getVal()
+#     return a / b
+
+# def ment() :
+#     print("사칙연산을 수행합니다. 연산번호(1:add, 2:sub, 3:mul, 4:div, 5:end")
+    
+# while True :
+#     ment()
+#     choice = int(input("연산 번호 : "))
+#     if choice == 1 :
+#         print(add())
+#     elif choice == 2 :
+#         print(sub())
+#     elif choice == 3 :
+#         print(mul())
+#     elif choice == 4 :
+#         print(div())
+#     elif choice == 5 :
+#         print('end')
+#     else : print("Wrong num, retry!")
