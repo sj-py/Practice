b80001e6fe5b082bae0bae404d01b5e8af8411ff
Seongjin Kim <mansj98@naver.com>
2023-05-25 21:51:51 +0900

    practice

--- 프로그래머스/practice.py
+++ 프로그래머스/practice.py
@@ -1,11 +1,10 @@
 def solution(numer1, denom1, numer2, denom2):
     answer = []
-    print(max(denom1,denom2),denom1*denom2+1)
     for i in range(max(denom1,denom2),denom1*denom2+1):
-        if denom1%i==0 and denom2%i==0:
-            answer.append(int((numer1*(denom1/i))+(numer2*(denom2/i))))
+        if i%denom1==0 and i%denom2==0:
+            answer.append(int((numer1*(i/denom1)+(numer2*(i/denom2)))))
             answer.append(i)
             break
     return answer
 
-print(solution(1,2,3,4))
+print(solution(1,1,1,1))
